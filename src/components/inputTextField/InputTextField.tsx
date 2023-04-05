5644301927c42769b8704839f1327b3fc707187f
Rodrigo Cardoso <rodrigo.cardoso1007@gmail.com>
2023-04-04 22:14:10 -0300

    TextInput adjustments (#3)
    * Navigation development
    
    * Revert "Navigation development"
    
    This reverts commit 8ea84b12cfe680c1109053ad89c119c92dcebbbd.
    
    * Ajustes no component de input

--- src/components/inputTextField/InputTextField.tsx
+++ src/components/inputTextField/InputTextField.tsx
@@ -2,17 +2,22 @@ import {
     View, TextInput,
     KeyboardTypeOptions, StyleProp, 
     TextStyle, ViewStyle, 
-    ColorValue 
+    ColorValue, 
+    Text
 } from "react-native";
-import { Style } from "./InputTextField.style";
+import { style } from "./InputTextField.style";
 
 interface IInputTextField {
     value?: string,
+    label?: string,
     placeholder?: string,
     keyboardType?: KeyboardTypeOptions,
     maxLength?: number,
     numberOfLines?: number,
-    contentStyle?: StyleProp<ViewStyle>,
+    containerStyle?: StyleProp<ViewStyle>,
+    containerLabel?: StyleProp<ViewStyle>,
+    containerInputStyle?: StyleProp<ViewStyle>,
+    labelStyle?:  StyleProp<TextStyle>,
     textStyle?:  StyleProp<TextStyle>,
     placeholderTextColor?: ColorValue,
     secureTextEntry?: boolean,
@@ -22,25 +27,34 @@ interface IInputTextField {
 function InputTextField (props: IInputTextField){
     const {
         value, placeholder, 
-        keyboardType, maxLength, 
-        numberOfLines, textStyle, 
-        contentStyle, placeholderTextColor,
-        secureTextEntry, onChange
+        label, keyboardType,
+        maxLength, numberOfLines, 
+        textStyle, labelStyle, 
+        containerStyle, placeholderTextColor, 
+        secureTextEntry, containerInputStyle, 
+        containerLabel, onChange
     } = props;
 
     return (
-        <View style={[Style.container ,contentStyle]}>
-            <TextInput
-                value={value}
-                placeholder={placeholder}
-                placeholderTextColor={placeholderTextColor}
-                keyboardType={keyboardType}
-                maxLength={maxLength}
-                numberOfLines={numberOfLines}
-                style={[Style.text, textStyle]}
-                onChangeText={onChange}
-                secureTextEntry={secureTextEntry}
-            />
+        <View style={[style.container, containerStyle]}>
+            {label && 
+                <View style={containerLabel}>
+                    <Text style={[style.label, labelStyle]}>{label}</Text>
+                </View>
+            }
+
+            <View style={[style.containerInput, containerInputStyle]}>
+                <TextInput
+                    value={value}
+                    placeholder={placeholder}
+                    placeholderTextColor={placeholderTextColor || style.placeholder.color}
+                    keyboardType={keyboardType}
+                    maxLength={maxLength}
+                    numberOfLines={numberOfLines}
+                    style={[style.text, textStyle]}
+                    onChangeText={onChange}
+                    secureTextEntry={secureTextEntry} />
+            </View>
         </View>
     )
 }
